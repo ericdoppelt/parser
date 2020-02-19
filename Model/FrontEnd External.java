15ead1efccdf1582fa4e8007743f09d91c2e1303
Erik Gregorio <eg158@duke.edu>
2020-02-19 00:58:37 -0500

    Modified UserInterface interface

--- Model/FrontEnd External.java
+++ Model/FrontEnd External.java
@@ -5,7 +5,7 @@ import java.util.*;
  * This interface was developed to give a general idea of how the front end will look like to an outside class.
  * In general, the front end will simply take a in a string and deliver it to the controller to be processed.
  */
-public interface FrontEnd External {
+public interface UserInterface {
 
     /**
      * This method allows a given classs to retrive the command that has just been given the the view
@@ -15,8 +15,9 @@ public interface FrontEnd External {
     /**
      * This method allows the controller/model to trigger an error warning in case their is any error with user-given
      * string.
+     * @Param Error message that will be displayed
      */
-    public void throwError();
+    public void throwError(Error E);
 
     /**
      * This method allows a class to send the result of a command back to the view.
