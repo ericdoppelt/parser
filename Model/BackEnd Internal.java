33513d1412a2bafd3b2ec2857e35ef86d28a3aec
lubenjamin <lubenjamin@gmail.com>
2020-02-18 22:36:16 -0500

    added interfaces

---
+++ Model/BackEnd Internal.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public interface BackEnd Internal {
+
+    /**
+     * 
+     */
+    public void makeInfoPackage();
+
+    /**
+     * 
+     */
+    public void executeCommand();
+
+    /**
+     * 
+     */
+    public void addHistory();
+
+    /**
+     * 
+     */
+    public void getHistory();
+
+}
