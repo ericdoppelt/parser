7b17ae637a3ab422e1a8acf71f8b97b0fae7414c
lubenjamin <lubenjamin@gmail.com>
2020-03-05 16:33:39 -0500

    made file reading better

--- src/slogo/Model/FileManager/FileTester.java
+++ src/slogo/Model/FileManager/FileTester.java
@@ -1,22 +1,36 @@
 package slogo.Model.FileManager;
 
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileTester {
     public static void main(String args[]){
+        try {
+            FileWriter test = new FileWriter();
+            Map<String, String> configMap = new HashMap<>();
+            configMap.put("image", "DUVALL");
+            configMap.put("language", "FRENCH");
+            configMap.put("background", "0xff");
+            configMap.put("pen", "0xAB");
 
-        FileWriter test = new FileWriter();
-        List<String> parameters = new ArrayList<>();
-        parameters.add("image");
-        parameters.add("language");
-        parameters.add("background");
-        parameters.add("pen");
-        test.saveConfig(parameters);
+            test.saveConfig(configMap, "TestConfig");
 
-        FileReader testReader = new FileReader("TEST.xml");
-        String parameter = testReader.getString("image");
-        System.out.println(parameter);
-
+            FileReader testReader = new FileReader("TestConfig.xml");
+            testReader.getConfigMap();
+        } catch(XMLException e){
+            popUp();
+        }
+    }
+    private static void popUp(){
+        String errorMessage = "File is invalid";
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("XML Error");
+        alert.setHeaderText(errorMessage);
+        Platform.runLater(alert::showAndWait);
     }
 }
